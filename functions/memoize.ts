b955e55a8bf9cd0794010fa2466480325d4b4dfb
ling <361864462@qq.com>
2025-04-01 14:35:29 +0800

    ts修改

--- functions/memoize.ts
+++ functions/memoize.ts
@@ -1,17 +1,26 @@
-// declare function memoize<T>(): RereturnType<T>
 
-function memoize<Function>(fn: Function): (...args: any[]) => any {
-    const cache = new Map()
-    return function (...args) {
+function memoize<T extends (...args: any[]) => any>(fn: T): T {
+    const cache = new Map<string, ReturnType<T>>()
+    return function(this: unknown, ...args: Parameters<T>): ReturnType<T> {
         const key = JSON.stringify(args)
+
         if (cache.has(key)) {
-            return cache.get(key)
+            return cache.get(key)!
         }
-        if (typeof fn !== 'function') return
-        const result = fn(...args)
+
+        const result = fn.apply(this, args)
         cache.set(key, result)        
         return result
-    }
+    } as T
 }
+
+// 使用示例
+// const add = memoize((a: number, b: number): number => {
+//     console.log('Calculating...');
+//     return a + b;
+// });
+
+// console.log(add(1, 2)); // 计算并缓存
+// console.log(add(1, 2)); // 从缓存读取
 
 
